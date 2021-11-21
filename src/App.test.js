ca3e69fb82e6b641295fb0e8f63ce690cd069273
Ronald Arias <ariasron@hotmail.com>
2021-11-21 15:59:36 -0500

    Creates a general reset for css global styles with styled components

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders hello world', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const headingElement = screen.getByText(/hello world/i)
+  expect(headingElement).toBeInTheDocument()
 })
