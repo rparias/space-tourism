093e502def5ff5077de01de411afa07b0f7937c9
Ronald Arias <ariasron@hotmail.com>
2021-11-29 20:21:57 -0500

    Installs React router dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react'
 import GlobalStyle from './globalStyles'
 import DesignSystem from './DesignSystem'
 import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Home />
-      <DesignSystem />
+      <Router basename="/space-tourism">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/design" element={<DesignSystem />} />
+        </Routes>
+      </Router>
     </>
   )
 }
