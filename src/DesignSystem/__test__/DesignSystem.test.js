f76ebe3557f19ee6d4988059b1b099dc13706f47
Ronald Arias <ariasron@hotmail.com>
2021-11-29 21:28:26 -0500

    fix(test): wrapper DesignSystem test on Memory Router

--- src/DesignSystem/__test__/DesignSystem.test.js
+++ src/DesignSystem/__test__/DesignSystem.test.js
@@ -1,10 +1,11 @@
 import { React } from 'react'
 import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 import DesignSystem from '../DesignSystem'
 
 describe('Design System', () => {
   it('should render a design system', () => {
-    render(<DesignSystem />)
+    render(<DesignSystem />, { wrapper: MemoryRouter })
     expect(screen.getByText('Design system')).toBeInTheDocument()
   })
 })
